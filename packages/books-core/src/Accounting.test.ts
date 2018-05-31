57434e516c0181982c12f13151ab089ea36bcf00
Zachary Jones <zachary@thorough.company>
2018-05-31 07:04:01 -0400

    Playing around with KISS approaches to transactions.

--- packages/books-core/src/Accounting.test.ts
+++ packages/books-core/src/Accounting.test.ts
@@ -1,13 +1,40 @@
 import { Accounting } from './Accounting';
 
-describe('Accounting class', () => {
-  describe('Accounting Equation', () => {
-    test('Assets = Liabilities + Equity', () => {
-      const accounting = new Accounting();
+function expectAccountingEquationSatisfied(accounting: Accounting) {
+  expect(accounting.assets).toEqual(
+    accounting.liabilities + accounting.equity
+  );
+}
 
-      expect(accounting.assets).toEqual(
-        accounting.liabilities + accounting.equity
-      );
-    });
+describe('Accounting class', () => {
+  let accounting: Accounting;
+
+  beforeEach(() => {
+    accounting = new Accounting();
+  });
+
+  afterEach(() => {
+    expectAccountingEquationSatisfied(accounting);
+  });
+
+  test('Initial values', () => {
+    expect(accounting.assets).toEqual(0);
+    expect(accounting.liabilities).toEqual(0);
+    expect(accounting.equity).toEqual(0);
+    expectAccountingEquationSatisfied(accounting);
+  });
+
+  xtest('Cash account is considered part of Assets', () => {});
+  xtest('Common Stock is considered part of Equity', () => {});
+
+  test('Selling common stock', () => {
+    const amount = 500;
+
+    accounting.addTransaction({ cash: amount, commonStock: amount });
+
+    expect(accounting.cash).toEqual(amount);
+    expect(accounting.assets).toEqual(amount);
+    expect(accounting.equity).toEqual(amount);
+    expect(accounting.commonStock).toEqual(amount);
   });
 });
--- packages/books-core/src/Accounting.ts
+++ packages/books-core/src/Accounting.ts
@@ -1,5 +1,21 @@
+interface ITransaction {
+  cash: number;
+  commonStock: number;
+}
+
 export class Accounting {
-  public liabilities: number = 0;
   public assets: number = 0;
+  public cash: number = 0;
+  public commonStock: number = 0;
   public equity: number = 0;
+  public liabilities: number = 0;
+
+  addTransaction(details: ITransaction) {
+    const { cash, commonStock } = details;
+
+    this.cash += cash;
+    this.assets += cash;
+    this.equity += commonStock;
+    this.commonStock += commonStock;
+  }
 }
